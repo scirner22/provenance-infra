4b042a4f6d0d58f854c599111a5ac8a48f06153a
mlatimer <mlatimer@figure.com>
2022-01-13 11:38:15 -0700

    updated variable for workspaces

--- axelar/infrastructure/keypair.tf
+++ axelar/infrastructure/keypair.tf
@@ -2,6 +2,6 @@
 # /Volumes/Keybase/team/figureops/axelar/
 resource "aws_key_pair" "ssh" {
   key_name        = terraform.workspace
-  public_key      = var.mainnet_public_key
+  public_key      = terraform.workspace == "axelar-testnet" ? "" : var.mainnet_public_key
 
 }
