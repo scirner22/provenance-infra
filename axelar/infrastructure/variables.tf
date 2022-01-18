626634fcb8f438119f93a15dd2d3ee1d5de227ef
mlatimer <mlatimer@figure.com>
2022-01-18 12:22:48 -0700

    updated axelar-testnet

--- axelar/infrastructure/variables.tf
+++ axelar/infrastructure/variables.tf
@@ -3,3 +3,9 @@ variable "mainnet_public_key" {
   type        = string
   default     = ""
 }
+
+variable "testnet_public_key" {
+  description = "The public key material."
+  type        = string
+  default     = ""
+}
